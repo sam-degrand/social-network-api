d1bdab5f57e85ce78a0ab905e3b92ab9e4504876
Sam DeGrandchamp <degrandchamps@gmail.com>
2023-08-14 18:49:31 -0400

    added controllers, started index.js model

---
+++ models/User.js
@@ -0,0 +1,28 @@
+const {Schema, model} = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please enter a valid email']
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Thought'
+      }
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+      }
+    ]
+  });
