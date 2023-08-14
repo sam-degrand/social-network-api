47129fd7af218f98b5de58776b4e6ff68233d79d
Sam DeGrandchamp <degrandchamps@gmail.com>
2023-08-14 19:34:47 -0400

    moved the routes into a folder, added index.js

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,18 @@
+const router = require("express").Router();
+
+const {
+  getAllThoughts,
+  getThoughtById,
+  createThought,
+  updateThought,
+  deleteThought,
+} = require("../../controller/thoughtController");
+// Set up GET all and POST at /api/thoughts
+router.route("/").get(getAllThoughts).post(createThought);
+
+// Set up GET one, PUT, and DELETE at /api/thoughts/:id
+router
+  .route("/:id")
+  .get(getThoughtById)
+  .put(updateThought)
+  .delete(deleteThought);
--- routes/thoughtRoutes.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-
-const {
-  getAllThoughts,
-  getThoughtById,
-  createThought,
-  updateThought,
-  deleteThought,
-} = require("../../controller/thoughtController");
